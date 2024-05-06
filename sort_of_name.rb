299bc5161970aed45ee447a59210a8e1144a2255
Vincent Thomas <vincent.n.thomas1@gmail.com>
2024-05-06 13:29:55 +0200

    docs

--- sort_of_name.rb
+++ sort_of_name.rb
@@ -2,6 +2,13 @@
 def sort_array(name)
   return name.sort_by { |name| name.split("=")[1] }
 end
+
+# file_name
+# Är ansvarig för att ta en array av strängar och kunna skriva de till filsystemet.
+# Argukent
+# - names: Type Array<String>, själva texten som ska writeas till filen. indexen agerar som rader i filen.
+# Av: Anton Angervall & Vincent Thomas
+# Datum: 2024-05-06
 def file_name(names)
   string = ''
   index = 0 
@@ -16,6 +23,12 @@ def file_name(names)
     fa.write(string)
   end
 end
+
+# new_array
+# Wrapper funktion för att kunna läsa filen för att ta input, och sedan sortera med hjälp av en annan funktion som sedan kan writeas till disken.
+# Argument: Inga
+# Datum: 2024-05-06
+# By: Vincent Thomas & Anton Angervall
 def new_array()
   file = File.readlines("players.txt", chomp: true)
   new_array = []
